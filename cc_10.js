fad887c5fa4ede1d1153a1cdc694b3534a8a38f1
Tuong Nguyen <yolotube21@yahoo.com>
2025-02-19 13:04:41 -0500

    Task 4 - Implementing Order Management

--- cc_10.js
+++ cc_10.js
@@ -33,7 +33,7 @@ class Order {
     getOrderDetails(){
         const totalPrice = this.product.price * this.quantity
         return `Order ID: ${this.orderId}, Product: ${this.product.name}, Quantity: ${this.quantity}, Total Price: $${totalPrice}`;
-    }// Calculates the total price of order
+    }
 };
 const order1 = new Order(501, prod1, 2);
 console.log(order1.getOrderDetails()); 
@@ -47,13 +47,41 @@ class Inventory {
         this.products = []
     }
     addProducts(product) {
-        this.products.push(product);// Adds new product to the inventory
+        this.products.push(product);// Adds new product to the inventory from the Order class
     }
     listProducts(){
         this.products.forEach(product => console.log(product.getDetails()));
-    }// Logs product details
+    }// Logs product details from Order class
+// Task 4 - Implementing Order Management
+    orders = [];
+    addProduct(product){
+        this.products.push(product);
+    }
+    listProducts() {
+        this.products.forEach(product => console.log(product.getDetails()))
+    }
+    placeOrder(orderId, product, quantity){
+        if (product.stock > quantity) {
+            const order = new Order(orderId, product, quantity)// Creates a new order and adds it to the order list if stock is available
+            this.orders.push(order)
+        }//If stock is greater than order quantity, creates a new Order object and adds it to orders array
+        else {
+            console.log(`Order ${orderId} Failed: Insufficient stock for ${product.name}.`)
+        }// Ensures that the order will not be placed if there is not enough stock for the order
+    }    
+    listOrders(){
+        this.orders.forEach(order => console.log(order.getOrderDetails()))
+    }
+
 }
+// Task 3 Test Case
 const inventory = new Inventory();
 inventory.addProduct(prod1);
 inventory.listProducts();
 // Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 5"
+// Task 4 Test Cases
+inventory.placeOrder(601, prod1, 2);
+inventory.listOrders();
+// Expected output: "Order ID: 601, Product: Laptop, Quantity: 2, Total Price: $2400"
+console.log(prod1.getDetails());
+// Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 3"
