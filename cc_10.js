40dd21f6704666c0b4a89147bfba21d49db68b3c
Tuong Nguyen <yolotube21@yahoo.com>
2025-02-18 22:56:28 -0500

    Task 2 - Creating an Order Class

--- cc_10.js
+++ cc_10.js
@@ -20,3 +20,23 @@ const prod1 = new Product("Laptop", 101, 1200, 10);
 console.log(prod1.getDetails());// Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 10"
 prod1.updateStock(3);
 console.log(prod1.getDetails()); // Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 7"
+
+// Task 2 - Creating an Order Class
+class Order {
+    constructor(orderId, product, quantity) {
+        this.orderId = orderId,
+        this.product = product,
+        this.quantity = quantity
+
+        this.product.updateStock(this.quantity)// Reduce stock when order is placed
+    }
+    getOrderDetails(){
+        const totalPrice = this.product.price * this.quantity
+        return `Order ID: ${this.orderId}, Product: ${this.product.name}, Quantity: ${this.quantity}, Total Price: $${totalPrice}`;
+    }// Calculates the total price of order
+};
+const order1 = new Order(501, prod1, 2);
+console.log(order1.getOrderDetails()); 
+// Expected output: "Order ID: 501, Product: Laptop, Quantity: 2, Total Price: $2400"
+console.log(prod1.getDetails()); 
+// Expected output: "Product: Laptop, ID: 101, Price: $1200, Stock: 5" (Stock reduced)
